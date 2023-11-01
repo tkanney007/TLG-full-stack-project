610240b3df31f4c566ebec323ef25f26fe31a94e
KPP3 <137243573+KPP3@users.noreply.github.com>
2023-11-01 17:43:02 -0500

    improve

--- frontend/easybills/src/App.js
+++ frontend/easybills/src/App.js
@@ -2,8 +2,8 @@ import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import CustomNavbar from "../components/Navbar";
-import FooterCustom from "../components/Footer";
+import CustomNavbar from "./components/Navbar";
+import FooterCustom from "./components/Footer";
 
 
 function App() {
