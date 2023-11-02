ef6741d89a237feccb77e32cee28ee90fd71f07f
KPP3 <137243573+KPP3@users.noreply.github.com>
2023-11-01 19:56:52 -0500

    changed js to jsx

--- frontend/easybills/src/App.jsx
+++ frontend/easybills/src/App.jsx
@@ -1,9 +1,11 @@
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import { Route, Routes } from "react-router-dom";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import CustomNavbar from "./components/Navbar";
 import FooterCustom from "./components/Footer";
+import Bills from "./pages/Bills";
 
 
 function App() {
@@ -12,7 +14,9 @@ function App() {
     <div>
       <CustomNavbar />
       <Routes>
-        <Route></Route>
+      <Route path='/' element={<Home/>} />
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/bills' element={<Bills/>}/>
       </Routes>
       <FooterCustom />
     </div>
