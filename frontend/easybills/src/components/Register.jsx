0428944e9566557f8cd581cd1d22ad6be1207794
LeoSOSAPONS <137127243+LeoSOSAPONS@users.noreply.github.com>
2023-11-02 18:38:46 -0500

    adjustments to nav and footer

---
+++ frontend/easybills/src/components/Register.jsx
@@ -0,0 +1,46 @@
+import { Button, Checkbox, Label, TextInput } from "flowbite-react";
+import Link from "next/link";
+
+function Component() {
+  return (
+    <form className="flex max-w-md flex-col gap-4">
+      <div>
+        <div className="mb-2 block">
+          <Label htmlFor="email2" value="Your email" />
+        </div>
+        <TextInput
+          id="email2"
+          type="email"
+          placeholder="name@flowbite.com"
+          required
+          shadow
+        />
+      </div>
+      <div>
+        <div className="mb-2 block">
+          <Label htmlFor="password2" value="Your password" />
+        </div>
+        <TextInput id="password2" type="password" required shadow />
+      </div>
+      <div>
+        <div className="mb-2 block">
+          <Label htmlFor="repeat-password" value="Repeat password" />
+        </div>
+        <TextInput id="repeat-password" type="password" required shadow />
+      </div>
+      <div className="flex items-center gap-2">
+        <Checkbox id="agree" />
+        <Label htmlFor="agree" className="flex">
+          I agree with the&nbsp;
+          <Link
+            href="#"
+            className="text-cyan-600 hover:underline dark:text-cyan-500"
+          >
+            terms and conditions
+          </Link>
+        </Label>
+      </div>
+      <Button type="submit">Register new account</Button>
+    </form>
+  );
+}
