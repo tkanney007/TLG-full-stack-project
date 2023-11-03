ffd55511741733f9db7d4289cd4d3f471e72abc0
KPP3 <137243573+KPP3@users.noreply.github.com>
2023-11-03 17:30:06 -0500

    a

--- frontend/easybills/src/pages/Register.jsx
+++ frontend/easybills/src/pages/Register.jsx
@@ -96,9 +96,6 @@ const Register = () => {
                   className="block w-full rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
-              </div>
-
-  
               <div>
                 <Link to="/budget">
                 <button
