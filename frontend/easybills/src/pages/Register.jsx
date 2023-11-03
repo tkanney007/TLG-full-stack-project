6cb7b2c89661ae303c57b88d27dceac1e9b5894c
KPP3 <137243573+KPP3@users.noreply.github.com>
2023-11-03 17:34:08 -0500

    removed div

--- frontend/easybills/src/pages/Register.jsx
+++ frontend/easybills/src/pages/Register.jsx
@@ -96,6 +96,7 @@ const Register = () => {
                   className="block w-full rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
+
               <div>
                 <Link to="/budget">
                 <button
@@ -104,17 +105,7 @@ const Register = () => {
                   Register
                 </button>
                 </Link>
-
               </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Register
-              </button>
             </div>
           </form>
         </div>
